2e54660faca4e42b385c71c78c4d8676446f9bee
Matheus Tonon <math.tonon@gmail.com>
2026-03-18 05:43:46 -0300

    feat: add custom favicon

--- app/payment/page.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamic = "force-dynamic";
-
-import PaymentPageClient from '@/components/payment/payment-section'
-
-export default function Page() {
-  return <PaymentPageClient />
-}
